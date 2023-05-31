b25246291f582876bc2d044425b5d76e49eae4df
Replit user <>
2023-05-31 08:18:14 +0000

    ok_imma_just_upload_this_myself_to_github

--- gitHelper.sh
+++ gitHelper.sh
@@ -5,12 +5,6 @@ echo =**GitHelper by Efendo**=
 echo
 git add $1
 git commit -m $2
-
-if [$3 = "true"] then
 git push
 echo GitHelper: pushed $1 with message $2
-fi
-if [$3 = "false"] then
-echo GitHelper: commited $1 with message $2, push manually
-fi
 echo

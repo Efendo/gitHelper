3dc4dc8d68fed75f3dea9e2391abec4bc4ccb646
Replit user <>
2023-05-31 08:03:24 +0000

    update

--- gitHelper.sh
+++ gitHelper.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+echo
 echo =**GitHelper by Efendo**=
 echo
 git add $1
 git commit -m $2
+
+if [$3 = "true"] then
 git push
-echo 
 echo GitHelper: pushed $1 with message $2
+else
+echo GitHelper: commited $1 with message $2, to push use 'git push'
+fi
+
+echo 
